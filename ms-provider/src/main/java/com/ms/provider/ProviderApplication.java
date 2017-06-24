f1a83ef8c7cdb855abe1ea7d8327b27d77a8486a
luoqi <136428148@qq.com>
2017-06-24 15:22:47 +0800

    新建分支

--- ms-provider/src/main/java/com/ms/provider/ProviderApplication.java
+++ ms-provider/src/main/java/com/ms/provider/ProviderApplication.java
@@ -11,7 +11,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class ProviderApplication {
     public static void main(String[] args) {
-
         new SpringApplicationBuilder(ProviderApplication.class).web(true).run(args);
     }
 }
